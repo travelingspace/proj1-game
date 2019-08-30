83cce82f17ac0715c302df9fd0092a8d50fde91c
James Houck <jameshouck@live.com>
2019-08-30 12:59:59 -0500

    updates

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import random
 
-def shuffle()
-    var 
+card = ["1","2","3","4","5","6","7","8","9", "10","J","Q","K","A"]
+suit = ["spades", "hearts", "clubs", "diamonds"]
+
+def shuffle():
+        
+    for i in range(len(card)):
+        print(card[i],"\n")
+    return
+
+shuffle()

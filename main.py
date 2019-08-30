b914b5650b2c038b26a4e7c8f66a54721de18c8f
James Houck <jameshouck@live.com>
2019-08-30 13:09:37 -0500

    updates

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import random
 
 card = ["1","2","3","4","5","6","7","8","9", "10","J","Q","K","A"]
 suit = ["spades", "hearts", "clubs", "diamonds"]
+deck = []
 
-def shuffle():
-        
+def BuildDeck():
+    deck = []
     for i in range(len(card)):
-        print(card[i],"\n")
-    return
+        for ind in range(len(suit))
+            deck.append(card[i], " of ", suit[ind])
+    for i in range(len(deck))
+        print(deck[i],"\n")
+    return deck
 
-shuffle()
+BuildDeck()

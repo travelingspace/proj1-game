6d1a5fdebbf05daf9dd3663f98bf2f21af27daa3
James Houck <jameshouck@live.com>
2019-08-30 13:29:07 -0500

    can create a deck and shuffle it now

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import random
 
 card = ["1","2","3","4","5","6","7","8","9", "10","J","Q","K","A"]
 suit = ["spades", "hearts", "clubs", "diamonds"]
-deck = []
 
 def BuildDeck():
     deck = []
     for i in range(len(card)):
         for ind in range(len(suit)):
-            deck.append(card[i], " of ", suit[ind])
-    for i in range(len(deck)):
-        print(deck[i],"\n")
+            cardName = card[i] + " of " + suit[ind]
+            deck.append(cardName)
     return deck
 
-BuildDeck()
+def shuffleDeck(deck):
+    random.shuffle(deck)
+    return deck
+
+deck = BuildDeck()
+
+newDeck = shuffleDeck(deck)
+for i in range(len(newDeck)):
+        print(deck[i],"\n")
+
+exit()
